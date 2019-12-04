b23509711114c286a44491f0034d5babee8a8c35
amitp <amitpatel_7@msn.com>
2019-12-04 00:53:33 -0800

    adjusted entrySkills variable

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -4,7 +4,7 @@ import { IndexPageTemplate } from '../../templates/index-page'
 
 const IndexPagePreview = ({ entry, getAsset }) => {
   const data = entry.getIn(['data']).toJS()
-  const entrySkills = entry.getIn(['data', 'skills_list', 'skill'])
+  const entrySkills = entry.getIn(['data', 'skills_list', 'skills'])
   const skills = entrySkills ? entrySkills.toJS() : []
 
   if (data) {
