ec928d94e1c15461ccde245d3bc792922744ec21
amitp <amitpatel_7@msn.com>
2020-01-06 10:16:19 -0800

    moved skills box inside .intro

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -23,16 +23,17 @@ const Hero = ({ background_image, title, subtitle, skillItems }) => (
     }/>
     <div className='intro animate fadeInLeft one'>
       <h1 className='hero_title'>{title}</h1>
-      <h3 className='hero_subtitle'>{subtitle}</h3>    
+      <h3 className='hero_subtitle'>{subtitle}</h3>
+      <fieldset className='skills_list_container'>
+        <legend>Skills</legend>
+        {skillItems.map(({ skill }, keys) => {
+          return (
+            <h4 key={keys} className='skill'>{skill}</h4>
+          )
+        })}
+      </fieldset>
     </div>
-    <fieldset className='skills_list_container'>
-      <legend>Skills</legend>
-      {skillItems.map(({ skill }, keys) => {
-        return (
-          <h4 key={keys} className='skill'>{skill}</h4>
-        )
-      })}
-    </fieldset>
+
   </section>
 )
 
