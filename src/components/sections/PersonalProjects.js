1498fbc93acb1e50750f61944457447c86046e21
amitp <amitpatel_7@msn.com>
2020-01-09 11:26:45 -0800

    created PersonalProjects component and added image widget for section background

---
+++ src/components/sections/PersonalProjects.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const PersonalProjects = () => {
+  return (
+    <div className="personal_projects_section"></div>
+  )
+}
+
+export default PersonalProjects
