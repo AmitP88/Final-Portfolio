80d3f9e689561a9ba05e748cd11de362af16981a
amitp <amitpatel_7@msn.com>
2020-01-11 18:57:43 -0800

    added stuff to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,15 +5,19 @@ const Footer = class extends React.Component {
   render() {
     return (
       <footer>
-        <a href="#" className="footer-icon">
-          <FontAwesomeIcon icon={['fab', 'github']} size="2x" />        
-        </a>
-        <a href="#" className="footer-icon">
-          <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" />        
-        </a>
-        <a href="#" className="footer-icon">
-          <FontAwesomeIcon icon={'envelope-square'} size="2x" />        
-        </a>
+        <p className="built-with">Built with</p>
+        <div className="icon-container">
+          <a href="#" className="footer-icon">
+            <FontAwesomeIcon icon={['fab', 'github']} size="2x" />        
+          </a>
+          <a href="#" className="footer-icon">
+            <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" />        
+          </a>
+          <a href="#" className="footer-icon">
+            <FontAwesomeIcon icon={'envelope-square'} size="2x" />        
+          </a>        
+        </div>
+        <p className="copyright">&copy; 2020 Amit </p>
       </footer>
     )
   }
