5485ac22b6964f7f308b27ebbecee31059c3f12f
amitp <amitpatel_7@msn.com>
2020-01-14 09:40:38 -0800

    removed clip-paths from middlestate images

--- src/components/sections/MiddleState.js
+++ src/components/sections/MiddleState.js
@@ -15,7 +15,7 @@ const MiddleState = ({ icon, icon_link, title, gridItems }) => {
       {gridItems.map(({ project = {image: '', title: '', tech_used: '', description: '', website_link: '', repo_link: ''} }, keys) => {
         return (
           <div key={keys} className="project_container" style={keys % 2 === 0 ? {flexDirection: 'row-reverse'} : {flexDirection: 'initial'}}>
-            <div className="column-1" style={keys % 2 === 0 ? {clipPath: 'polygon(20% 0%, 100% 0%, 100% 100%, 0% 100%, 20% 0%)'} : {clipPath: 'polygon(0% 0%, 80% 0%, 100% 100%, 0% 100%, 0% 0%)'}}>
+            <div className="column-1">
               <a href={project.website_link}>
                 <div className="col-1-background" style={
                   {
