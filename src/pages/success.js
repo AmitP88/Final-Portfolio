e0b62df08335773437b456cb19f5df210471e193
amitp <amitpatel_7@msn.com>
2020-01-17 20:07:50 -0800

    added gif and changed styles of header

--- src/pages/success.js
+++ src/pages/success.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Layout from '../../src/components/Layout'
 
-import message_received from '../../static/img/message_received.png'
+import message_received from '../../static/img/message_received.gif'
 
 const Success = props => (
   <Layout>
@@ -11,18 +11,9 @@ const Success = props => (
       <meta name="description" content="Success Page" />
     </Helmet>
 
-    <div id="main" className="alt">
-      <section id="one">
-        <div className="inner">
-          <header className="major">
-            <h1>Message received! I will get back to you as soon as possible! :)</h1>
-          </header>
-          <span className="image main">
-            <img src={message_received} alt="message_received" />
-          </span>
-          <p>Thank you for contacting us!</p>
-        </div>
-      </section>
+    <div className="success_container">
+      <h1>Message received! I'll get back to you as soon as possible! :)</h1>
+      <img src={message_received} alt="message_received" />
     </div>
   </Layout>
 );
