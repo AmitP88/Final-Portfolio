76e2dfefea1dd81400ef80d564e766d892bbda2c
amitp <amitpatel_7@msn.com>
2020-01-19 18:22:36 -0800

    changed meta title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Amit Patel - Front End Developer',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'This repo was built off of a NetlifyCMS Gatsby business starter that I used as the setup for my site.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
