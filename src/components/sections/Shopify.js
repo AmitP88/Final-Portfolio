3c25a06a3d0cec2e80c65e681d884d58b60f73bf
amit <amitpatel_7@msn.com>
2021-06-15 13:58:42 -0700

    changed Theme Certification title

--- src/components/sections/Shopify.js
+++ src/components/sections/Shopify.js
@@ -186,7 +186,7 @@ const Shopify = ({ icon, title, description, subsection_1_title, step_1_image, s
       <div className="certifications">
         <h3>Certifications</h3>
         <div className="cert">
-          <h4 className="cert-title">&#128311; Theme Development Certification</h4>
+          <h4 className="cert-title">&#128311; Theme Development</h4>
           <img src="/img/theme-certification.png" alt="theme cert" />        
         </div>
       </div>
